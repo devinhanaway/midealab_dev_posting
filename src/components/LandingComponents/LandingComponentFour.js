e7ad1407e32a422a3949165abaaf5412fca73ced
devin hanaway <devinh11@gmail.com>
2017-11-14 02:47:54 -0700

    fixed some of the mobile responsiveness within the eligibility section

--- src/components/LandingComponents/LandingComponentFour.js
+++ src/components/LandingComponents/LandingComponentFour.js
@@ -21,19 +21,22 @@ export default class LandingComponentThree extends Component{
 
             <div id="skills1" className="ui column centered header pad_bottom">
               <div id="absolute">
-                <i className="material-icons">&#xE90F;</i>
-                <h2>You’re an Inspired Developer </h2>
-                <p>Give you a set of specs and user stories and you will build an interface that not only meets the requirements, but anticipates user experience features that were missed. You have a deep appreciation for and understanding of the people and platforms you’re developing for.</p>
+
+                <i className="material-icons md-dark">favorite_border</i>
+
+                <h2>You aspire to more than just "build" </h2>
+                <p> You don’t just want to build product 9-5 for a paycheck. You want to continue to develop as a technologist. Learning new languages and adapting to different projects.</p>
               </div>
 
             </div>
 
           <div id="skills2" className="ui column centered header pad_bottom">
             <div id="absolute">
-              <i className="material-icons md-dark">favorite_border</i>
 
-              <h2>You aspire to more than just "build" </h2>
-              <p> You don’t just want to build product 9-5 for a paycheck. You want to continue to develop as a technologist. Learning new languages and adapting to different projects.</p>
+              <i className="material-icons md-dark">autorenew</i>
+              <h2>Process matter to you</h2>
+              <p>Commented git commits, testing, and remote workflow are second nature to you.  We love being small and scrappy and making it up as we go. You do too.</p>
+
             </div>
 
           </div>
@@ -62,9 +65,9 @@ export default class LandingComponentThree extends Component{
 
             <div id="skills5" className="ui column centered header pad_bottom">
               <div id="absolute">
-                <i className="material-icons md-dark">autorenew</i>
-                <h2>Process matter to you</h2>
-                <p>Commented git commits, testing, and remote workflow are second nature to you.  We love being small and scrappy and making it up as we go. You do too.</p>
+                <i className="material-icons">&#xE90F;</i>
+                <h2>You’re an Inspired Developer </h2>
+                <p>Give you a set of specs and user stories and you will build an interface that not only meets the requirements, but anticipates user experience features that were missed. You have a deep appreciation for and understanding of the people and platforms you’re developing for.</p>
               </div>
             </div>
 
--- src/components/LandingComponents/LandingComponentTwo.js
+++ src/components/LandingComponents/LandingComponentTwo.js
@@ -17,11 +17,11 @@ export default class LandingComponentTwo extends Component{
           <article className="landing2" id="section03">
 
 
-          <div className="ui three column grid stackable">
+            <div className="ui three column grid stackable">
               <div id="perk1" className="column">
                 <div className="ui segment container" id="absolute_nopadding">
                   {/* <img className="ui medium centered image" src="../../../images/peopleMap.jpg"/> */}
-                    <h2>Work From Anywhere or The Studio:</h2>
+                    <h2>Work From Anywhere    or from The Studio:</h2>
                   <p>Our dispersed workforce model allows you to work from wherever you want, though you’re welcome to join us within our development studio in downtown Lviv, and work alongside our portfolio companies and other creatives.</p>
                 {/* <h2>Startup + Development Studio</h2>
                   <p>Work on a client products and startups launched by Midealab's experienced founders.</p> */}
@@ -37,7 +37,7 @@ export default class LandingComponentTwo extends Component{
                   {/* <img className="ui medium centered image" src="../../../images/globalCitizen.jpg"/> */}
                 {/* <h2>Flexibility & Community</h2>
               <p>You will have the freedom to work from anywhere, but also have an office and startup culture to </p> */}
-                <h2>Competitive Compensation Startup Culture:</h2>
+                <h2>Competitive Compensation W/ Startup Culture:</h2>
               <p>Become part of startup studio community and become surrounded with world class developers, entrepreneurs and creatives. Consistent client work + opportunities to join internal startups that Midealab’s founders launch.</p>
               </div>
             </div>
